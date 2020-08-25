5fdb5d660925b9fa84e764875d3a8b1a299d7b78
blydiet <lydietoussaint123@gmail.com>
2020-08-25 16:56:50 -0400

    (feature-Use-FindLoacation)- I crated a date function and findLoaction function

--- src/app.js
+++ src/app.js
@@ -6,3 +6,20 @@ date = null,
 day = new Date(),
 dayOfWeek = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Today']
 
+
+function displayDate(){
+    let [month, date, year] = (day).toLocaleDateString().split("/")
+    return `${month}/${date}/${year}`
+}
+
+async function findLocation  (){
+    const {data} = await axios.get(`https://se-weather-api.herokuapp.com/api/v1/geo?zip_code=${zipcode}`)
+    latitude = data.latitude
+    longitude = data.longitude
+    
+    date = displayDate()
+    console.log(date)
+   }
+   
+   findLocation()
+   
