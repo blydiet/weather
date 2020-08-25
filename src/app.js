bf0e2930cc07c6629a98c6e12223e8b3eef92073
blydiet <lydietoussaint123@gmail.com>
2020-08-25 16:51:40 -0400

    (feature-varables-needForApi)- I added the varables that I may need for my Api to work

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 import { convertDate } from "./utils";
+let zipcode = 9120,
+latitude = null,
+longitude = null,
+date = null,
+day = new Date(),
+dayOfWeek = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Today']
 
-console.log("hello world");
